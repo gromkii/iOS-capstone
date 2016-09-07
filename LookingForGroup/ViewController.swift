674b42726b5acb8fc247876ed42fe214223c699f
Dax <limeside@gmail.com>
2016-09-07 13:01:22 -0500

    Initial commit. :rabbit:

---
+++ LookingForGroup/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  LookingForGroup
+//
+//  Created by Dax Richardson on 9/7/16.
+//  Copyright © 2016 Dax Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBAction func registerButton(sender: AnyObject) {
+    }
+    @IBAction func loginButton(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
